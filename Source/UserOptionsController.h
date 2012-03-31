67ff8648475fd291b306d45f62cf1b4afcf887ce
bob ackerman <obowah@gmail.com>
2012-03-30 20:47:43 -0700

    initial checkin of session dialog files - no code yet

---
+++ Source/UserOptionsController.h
@@ -0,0 +1,16 @@
+//
+//  UserOptionsController.h
+//  scout-desktop
+//
+//  Created by ackerman dudley on 3/30/12.
+//  Copyright (c) 2012 __SauceLabs__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface UserOptionsController : NSWindowController
+{
+    
+}
+
+@end
---
+++ Source/UserOptionsController.m
@@ -0,0 +1,34 @@
+//
+//  UserOptionsController.m
+//  scout-desktop
+//
+//  Created by ackerman dudley on 3/30/12.
+//  Copyright (c) 2012 __SauceLabs__. All rights reserved.
+//
+
+#import "UserOptionsController.h"
+
+@interface UserOptionsController ()
+
+@end
+
+@implementation UserOptionsController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
