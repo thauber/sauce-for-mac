3256b12fba0a1d4061f58e38ecbda7fa53b18039
bob ackerman <obowah@gmail.com>
2012-05-12 09:46:29 -0700

    class to run jar tunnel connector and display output

---
+++ Source/TunnelController.h
@@ -0,0 +1,37 @@
+//
+//  TunnelController.h
+//  scout
+//
+//  Created by ackerman dudley on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TunnelController : NSObject
+{    
+    NSPanel *panel;
+    NSButton *closeButton;
+    NSButton *hideButton;
+    NSTextView *infoTV;
+    BOOL hiddenDisplay;
+    NSFileHandle *fhand;
+    NSPipe *fpipe;
+}
+@property (assign) IBOutlet NSPanel *panel;
+@property (assign) IBOutlet NSButton *closeButton;
+@property (assign) IBOutlet NSButton *hideButton;
+@property (assign) IBOutlet NSTextView *infoTV;
+@property (assign) BOOL hiddenDisplay;
+@property(nonatomic, retain) NSFileHandle *fhand;
+@property(nonatomic, retain) NSPipe *fpipe;
+
+- (id)init;
+- (void)runSheetOnWindow:(NSWindow *)window;
+- (void)displayInfo:(NSString *)str;
+- (IBAction)doClose:(id)sender;
+- (IBAction)doHide:(id)sender;
+-(void)doTunnel;
+-(void)tunnelData: (NSNotification *) notif;
+-(void)endTunnel: (NSNotification *) notif;
+@end
---
+++ Source/TunnelController.m
@@ -0,0 +1,123 @@
+//
+//  TunnelController.m
+//  scout
+//
+//  Created by ackerman dudley on 5/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TunnelController.h"
+#import "ScoutWindowController.h"
+#import "SaucePreconnect.h"
+#import "AppDelegate.h"
+
+@implementation TunnelController
+@synthesize panel;
+@synthesize closeButton;
+@synthesize hideButton;
+@synthesize infoTV;
+@synthesize fhand;
+@synthesize fpipe;
+
+-(id)init
+{
+    if (self = [super init])
+    {
+        [NSBundle loadNibNamed:@"tunnel" owner:self];
+    }
+    return self;
+}
+
+- (void)runSheetOnWindow:(NSWindow *)window
+{
+    [NSApp beginSheet:panel modalForWindow:window modalDelegate:self
+       didEndSelector:nil   contextInfo:nil];
+    hiddenDisplay = NO;    
+}
+
+- (BOOL)hiddenDisplay
+{
+    return hiddenDisplay;
+}
+
+- (void)setHiddenDisplay:(BOOL)hidden;
+{
+    hiddenDisplay = hidden;
+}
+
+-(void)displayInfo:(NSString *)str
+{
+    if([str length])
+    {
+        [[[infoTV textStorage] mutableString] appendString: str];
+        NSRange range = NSMakeRange([[infoTV string] length], 0);
+        [infoTV scrollRangeToVisible: range];
+        NSRange textRange =[str rangeOfString:@"Connected!"];        
+        if(textRange.location != NSNotFound)    //Does contain the substring
+        {
+            [[NSApp delegate] tunnelConnected:YES];            
+        }           
+    }
+}
+
+- (IBAction)doClose:(id)sender
+{
+    [self endTunnel:nil];
+    [NSApp endSheet:panel];
+    [panel orderOut:self];
+    [[NSApp delegate] setTunnelCtrlr:nil];
+    [[NSApp delegate] toggleTunnelDisplay:NO];
+}
+
+- (IBAction)doHide:(id)sender
+{
+    [NSApp endSheet:panel];
+    [panel orderOut:self];
+    hiddenDisplay = YES;
+    [[NSApp delegate] toggleTunnelDisplay:YES];
+}
+
+- (void)doTunnel
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Sauce-Connect" ofType:@"jar"];
+    NSString *user = [[SaucePreconnect sharedPreconnect] user];
+    NSString *ukey = [[SaucePreconnect sharedPreconnect] ukey];
+    
+    NSString *farg = [NSString stringWithFormat:@"java -jar %@ %@ %@", path, user, ukey];
+    
+    NSTask *ftask = [[NSTask alloc] init];
+    self.fpipe = [NSPipe pipe];
+    [ftask setStandardOutput:fpipe];
+    [ftask setLaunchPath:@"/bin/bash"];
+    [ftask setArguments:[NSArray arrayWithObjects:@"-c", farg, nil]];
+    self.fhand = [fpipe fileHandleForReading];        
+    [[NSNotificationCenter defaultCenter] addObserver: self
+                                             selector: @selector(endTunnel:)
+                                                 name: NSTaskDidTerminateNotification
+                                               object: nil];
+    [[NSNotificationCenter defaultCenter] addObserver: self
+                                             selector: @selector(tunnelData:)
+                                                 name: NSFileHandleReadCompletionNotification
+                                               object: fhand];
+    [fhand readInBackgroundAndNotify];
+    [ftask launch];		// setup tunnel
+}
+
+-(void)tunnelData:(NSNotification *)notif
+{
+    NSData *data = [[notif userInfo] objectForKey: NSFileHandleNotificationDataItem];    
+    NSString *str = [[NSString alloc] initWithData: data encoding:NSASCIIStringEncoding];
+    NSLog(@"got data");
+    [self displayInfo:str];
+    [fhand readInBackgroundAndNotify];
+}
+
+-(void)endTunnel: (NSNotification *) notif
+{
+    [[NSNotificationCenter defaultCenter] removeObserver: self];
+    [fhand closeFile];    
+    self.fhand = nil;
+}
+
+
+@end
