01969a0485a885b570ea02ddd911f241d2d7de15
bob ackerman <obowah@gmail.com>
2012-05-29 16:55:30 -0700

    call window's 'submitBug' method on submit

--- Source/BugInfoController.m
+++ Source/BugInfoController.m
@@ -8,6 +8,7 @@
 
 #import "BugInfoController.h"
 #import "ScoutWindowController.h"
+#import "AppDelegate.h"
 
 @implementation BugInfoController
 @synthesize panel;
@@ -27,19 +28,20 @@
 {
     [NSApp beginSheet:panel modalForWindow:window modalDelegate:self
        didEndSelector:nil   contextInfo:nil];
-    [self retain];
 }
 
 - (IBAction)submit:(id)sender 
 {
-    [[ScoutWindowController sharedScout] submitBug:[title stringValue] desc:[description stringValue] to:[toFld stringValue]];
     [NSApp endSheet:panel];
     [panel orderOut:nil];
+    [[ScoutWindowController sharedScout] performSelectorOnMainThread:@selector(submitBug) withObject:nil waitUntilDone:NO]; 
 }
 
 - (IBAction)cancel:(id)sender 
 {
     [NSApp endSheet:panel];
     [panel orderOut:nil];
+    [[NSApp delegate] setBugCtrlr:nil];
+    [[NSApp delegate] performSelectorOnMainThread:@selector(showOptionsIfNoTabs) withObject:nil waitUntilDone:NO ];
 }
 @end
