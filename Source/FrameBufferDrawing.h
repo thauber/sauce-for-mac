b51abb0a4090c850ac877322684748d782b8a162
bob ackerman <obowah@gmail.com>
2013-01-22 10:22:32 -0800

    tweak for better display while resizing window

--- Source/FrameBufferDrawing.h
+++ Source/FrameBufferDrawing.h
@@ -455,29 +455,25 @@ printf("draw x=%f y=%f w=%f h=%f at x=%f y=%f\n", aRect.origin.x, aRect.origin.y
         r.size.height = size.height - r.origin.y;
     
     r.origin = aPoint;
-
-//    printf("draw:%d %d %d %d\n", (int)r.origin.x, (int)r.origin.y, (int)r.size.width, (int)r.size.height);
-//    printf("a:%d %d %d %d\n", (int)aRect.origin.x, (int)aRect.origin.y, (int)aRect.size.width, (int)aRect.size.height);
-
-    start = pixels + (int)(aRect.origin.y * size.width) + (int)aRect.origin.x;        
     
     if((aRect.size.width * aRect.size.height) > SCRATCHPAD_SIZE)
     {
         if(mHScale)        // scale the source size up
         {
-            aRect.size.width  = (int)(aRect.size.width * 1/mHScale);
-            aRect.size.height = (int)(aRect.size.height * 1/mVScale);
+            aRect.size.width  = MIN((int)(aRect.size.width * 1/mHScale),size.width);
+            aRect.size.height = MIN((int)(aRect.size.height * 1/mVScale),size.height);
             aRect.origin.y = 0;
-            r.origin.y = 0;
+            //r.origin.y = 0;
+            r = NSInsetRect(r, -r.origin.x, -r.origin.y);
         }
         start = pixels + (int)(aRect.origin.y * size.width) + (int)aRect.origin.x;
         bpr = size.width * sizeof(FBColor);
-//        printf("draw2:%d %d %d %d\n", (int)r.origin.x, (int)r.origin.y, (int)r.size.width, (int)r.size.height);
-//        printf("a2:%d %d %d %d\n", (int)aRect.origin.x, (int)aRect.origin.y, (int)aRect.size.width, (int)aRect.size.height);
         NSDrawBitmap(r, aRect.size.width, aRect.size.height, bitsPerColor, samplesPerPixel, sizeof(FBColor) * 8, bpr, NO, NO, NSDeviceRGBColorSpace, (const unsigned char**)&start);
     }
     else
     {
+        start = pixels + (int)(aRect.origin.y * size.width) + (int)aRect.origin.x;
+
         if(mHScale)        // scale the target size down to fit view
         {
             r.size.width  = (int)(r.size.width * mHScale);
@@ -485,8 +481,7 @@ printf("draw x=%f y=%f w=%f h=%f at x=%f y=%f\n", aRect.origin.x, aRect.origin.y
             r.origin.x = (int)(r.origin.x * mHScale);
             r.origin.y = (int)(r.origin.y * mVScale);
         }
-//        printf("draw2:%d %d %d %d\n", (int)r.origin.x, (int)r.origin.y, (int)r.size.width, (int)r.size.height);
-//        printf("a2:%d %d %d %d\n", (int)aRect.origin.x, (int)aRect.origin.y, (int)aRect.size.width, (int)aRect.size.height);
+
         FBColor* sp = scratchpad;
         int lines = aRect.size.height;
         while(lines--) {
