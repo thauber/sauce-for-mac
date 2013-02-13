2f5cd34cefcb739a49a30739f412d5473b116ddb
bob ackerman <obowah@gmail.com>
2013-02-13 11:29:16 -0800

    fix for centering when scaling

--- Source/centerclip.m
+++ Source/centerclip.m
@@ -52,18 +52,30 @@
         float scale = [[rfbvw fbuf] mHScale];
         maxX = docRect.size.width*scale - clipRect.size.width;
         maxY = docRect.size.height*scale - clipRect.size.height;
+        clipRect.origin = proposedNewOrigin;
+        if(docRect.size.width*scale < clipRect.size.width)
+            clipRect.origin.x = roundf(maxX / 2.0);
+        else
+            clipRect.origin.x = 0;
+        if(docRect.size.height*scale < clipRect.size.height)
+            clipRect.origin.y = roundf(maxY / 2.0);
+        else
+            clipRect.origin.y = 0;
     }
-    clipRect.origin = proposedNewOrigin;
-    
-    if (docRect.size.width < clipRect.size.width)
-        clipRect.origin.x = roundf(maxX / 2.0);
     else
-        clipRect.origin.x = roundf(MAX(0, MIN(clipRect.origin.x, maxX)));
-    
-    if (docRect.size.height < clipRect.size.height)
-        clipRect.origin.y = roundf(maxY / 2.0);
-    else
-        clipRect.origin.y = roundf(MAX(0, MIN(clipRect.origin.y, maxY)));
+    {
+        clipRect.origin = proposedNewOrigin;
+        
+        if (docRect.size.width < clipRect.size.width)
+            clipRect.origin.x = roundf(maxX / 2.0);
+        else
+            clipRect.origin.x = roundf(MAX(0, MIN(clipRect.origin.x, maxX)));
+        
+        if (docRect.size.height < clipRect.size.height)
+            clipRect.origin.y = roundf(maxY / 2.0);
+        else
+            clipRect.origin.y = roundf(MAX(0, MIN(clipRect.origin.y, maxY)));
+    }
     
     return clipRect.origin;
 }
